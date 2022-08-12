cf026402ce5fab1c66a0151dc3058e95f89483d6
Zoltán Máté <zoltan.mate@lombiq.com>
2022-08-12 16:18:58 +0200

    Removing unnecessary words

--- Lombiq.OrchardCoreApiClient/Interfaces/IOrchardCoreApi.cs
+++ Lombiq.OrchardCoreApiClient/Interfaces/IOrchardCoreApi.cs
@@ -15,7 +15,7 @@ public interface IOrchardCoreApi
     /// <param name="createTenantParameters">
     /// The necessary parameters to create a tenant: Name, RequestUrlPrefix etc.
     /// </param>
-    /// <returns>Returns the response of the tenant creation.</returns>
+    /// <returns>The response of the tenant creation.</returns>
     [Post("api/tenants/create")]
     [Header("Authorization", "Bearer")]
     Task<Response<string>> CreateAsync([Body] CreateApiViewModel createTenantParameters);
@@ -26,7 +26,7 @@ public interface IOrchardCoreApi
     /// <param name="setupTenantParameters">
     /// The necessary parameters to set up a tenant: Name, ConnectionString etc.
     /// </param>
-    /// <returns>Returns the response of the tenant setup.</returns>
+    /// <returns>The response of the tenant setup.</returns>
     [Post("api/tenants/setup")]
     [Header("Authorization", "Bearer")]
     Task<Response<string>> SetupAsync([Body] SetupApiViewModel setupTenantParameters);
