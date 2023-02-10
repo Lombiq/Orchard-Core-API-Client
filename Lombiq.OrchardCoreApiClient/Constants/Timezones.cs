a223efd6babff26ceb1a0158611d1a3df5a00a61
Benedek Farkas <benedek.farkas@lombiq.com>
2023-02-10 20:46:31 +0100

    Adding spell checking line ignore

--- Lombiq.OrchardCoreApiClient/Constants/Timezones.cs
+++ Lombiq.OrchardCoreApiClient/Constants/Timezones.cs
@@ -9,7 +9,7 @@ public static class Timezones
     private static IList<string> _timezoneIds;
     public static IList<string> TimezoneIds => _timezoneIds ??= GetTimeZoneIds();
 
-    public static IList<string> GetTimeZoneIds() => TzdbDateTimeZoneSource.Default
+    public static IList<string> GetTimeZoneIds() => TzdbDateTimeZoneSource.Default // #spell-check-ignore-line
         .ZoneLocations
         .Select(timeZone => timeZone.ZoneId)
         .ToList();
