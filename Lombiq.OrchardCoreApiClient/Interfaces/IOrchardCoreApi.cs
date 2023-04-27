d8912c28f015e75d0da9bc6dc0009c31a133d2c8
Krisztián Németh <krisztian.nemeth@lombiq.com>
2023-04-27 16:07:49 +0200

    Using custom action instead

--- Lombiq.OrchardCoreApiClient/Interfaces/IOrchardCoreApi.cs
+++ Lombiq.OrchardCoreApiClient/Interfaces/IOrchardCoreApi.cs
@@ -36,13 +36,9 @@ public interface IOrchardCoreApi
     /// </summary>
     /// <param name="editTenantParameters">The necessary parameter to edit a tenant: Name.</param>
     /// <returns>The response of the tenant edit.</returns>
-    [Post("api/tenants/edit")]
-    [Header("Authorization", "Bearer")]
-    Task<Response<string>> EditAsync([Body] TenantApiModel editTenantParameters);
-
     [Post("api/tenants/custom-edit")]
     [Header("Authorization", "Bearer")]
-    Task<Response<string>> CustomEditAsync([Body] TenantApiModel editTenantParameters);
+    Task<Response<string>> EditAsync([Body] TenantApiModel editTenantParameters);
 
     /// <summary>
     /// Remove a previously created tenant in Orchard Core.
