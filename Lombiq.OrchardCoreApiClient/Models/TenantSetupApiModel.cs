0f63199e51216dc59575d4e7cb47a9125a6193e5
Dávid El-Saig <david.el-saig@lombiq.com>
2023-06-25 22:18:02 +0200

    Obsolete stuff

--- Lombiq.OrchardCoreApiClient/Models/TenantSetupApiModel.cs
+++ Lombiq.OrchardCoreApiClient/Models/TenantSetupApiModel.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace Lombiq.OrchardCoreApiClient.Models;
@@ -23,6 +26,21 @@ public class TenantSetupApiModel
     [DataType(DataType.Password)]
     public string Password { get; set; }
     public string RecipeName { get; set; }
-    public IFormFile Recipe { get; set; }
+
+    [Obsolete($"This property relies on a deprecated NuGet package and will be removed in the next major version. " +
+              $"Please use the {nameof(RecipeJson)} property instead and serialize the value yourself in a project " +
+              $"that doesn't use the .NET Standard framework.")]
+    [JsonIgnore]
+    public IFormFile Recipe
+    {
+        get => RecipeJson.ToObject<IFormFile>();
+        set => JToken.FromObject(value);
+    }
+
+#pragma warning disable CS0618 // Use of Obsolete symbol. But only for reference.
+    [JsonProperty(nameof(Recipe))]
+#pragma warning restore CS0618 // Use of Obsolete symbol. But only for reference.
+    public JToken RecipeJson { get; set; }
+
     public string SiteTimeZone { get; set; }
 }
