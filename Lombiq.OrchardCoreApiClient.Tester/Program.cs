9bb18d7ad386bd97688de97012b9ee68a77c7b8e
Roland Márkus <roland.markus@lombiq.com>
2024-02-01 11:25:53 +0100

    Reverting supression.

--- Lombiq.OrchardCoreApiClient.Tester/Program.cs
+++ Lombiq.OrchardCoreApiClient.Tester/Program.cs
@@ -1,5 +1,6 @@
 using Lombiq.OrchardCoreApiClient.Models;
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,10 @@ public static class Program
     private const string ClientId = "Console";
     private const string ClientSecret = "Password";
 
+    [SuppressMessage(
+        "Design",
+        "CA1303:Do not pass literals as localized parameters",
+        Justification = "It is not a localization issue")]
     public static async Task Main(string[] arguments)
     {
         var port = int.TryParse(arguments.FirstOrDefault(), out var customPort) ? customPort : 44335;
@@ -52,11 +57,7 @@ public static class Program
             }
         );
 
-#pragma warning disable CA1303 // Do not pass literals as localized parameters
-        // CA1303 is a localization warning. We don't need that in Program.cs
         Console.WriteLine("Creating and setting up the tenant succeeded.");
-#pragma warning restore CA1303 // Do not pass literals as localized parameters
-
         var editModel = new TenantApiModel
         {
             Description = "Tenant edited by API Client",
@@ -69,9 +70,6 @@ public static class Program
         // Requires Orchard Core 1.6.0 or newer, on a 1.5.0 server this returns a 404 error.
         await apiClient.OrchardCoreApi.EditAsync(editModel);
 
-#pragma warning disable CA1303 // Do not pass literals as localized parameters
-        // CA1303 is a localization warning. We don't need that in Program.cs
         Console.WriteLine("Editing the tenant succeeded.");
-#pragma warning restore CA1303 // Do not pass literals as localized parameters
     }
 }
