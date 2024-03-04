4c78095b9dbe639b2a14d128153c98cd7cbc376a
Sára El-Saig <sara.el-saig@lombiq.com>
2024-03-04 02:21:30 +0100

    unusing

--- Lombiq.OrchardCoreApiClient/ConfigurableCertificateValidatingHttpClientHandler.cs
+++ Lombiq.OrchardCoreApiClient/ConfigurableCertificateValidatingHttpClientHandler.cs
@@ -5,7 +5,6 @@ using Refit;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
