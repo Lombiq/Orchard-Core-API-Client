a056418c1e9fbfb7442c7d6f94ad4d03981834e6
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2024-07-17 14:23:25 +0200

    Clarify some wording

--- Lombiq.OrchardCoreApiClient/Interfaces/IOrchardCoreApi.cs
+++ Lombiq.OrchardCoreApiClient/Interfaces/IOrchardCoreApi.cs
@@ -3,12 +3,12 @@ using System.Diagnostics.CodeAnalysis;
 namespace Lombiq.OrchardCoreApiClient.Interfaces;
 
 /// <summary>
-/// Marker interface for representing the subset of APIs of Orchard Core.
+/// Base interface for representing APIs of Orchard Core.
 /// </summary>
 [SuppressMessage(
     "Design",
     "CA1040:Avoid empty interfaces",
-    Justification = "This is a marker interface used to identify all Orchard Core API interfaces.")]
+    Justification = "This is a base interface used to identify all Orchard Core API interfaces May be extended in the future.")]
 public interface IOrchardCoreApi
 {
 }
