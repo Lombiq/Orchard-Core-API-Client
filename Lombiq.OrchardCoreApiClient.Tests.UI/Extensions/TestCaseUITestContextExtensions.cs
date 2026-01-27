a08d16eeaae4789d6f2de35a7aaa16b1e7275759
Sára El-Saig <sara.el-saig@lombiq.com>
2026-01-27 02:49:24 +0100

    Revert "Add delay."
    This reverts commit 332068f5fc740562331bf789641a3512c03f6e4a.

--- Lombiq.OrchardCoreApiClient.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.OrchardCoreApiClient.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -289,14 +289,8 @@ public static class TestCaseUITestContextExtensions
 
             using var response = await apiClient.OrchardCoreApi.SetupAsync(setupApiModel);
 
-            if (response.Error == null)
-            {
-                success = true;
-                continue;
-            }
-
+            if (response.Error == null) success = true;
             if (i == maxSetupAttempts - 1) CheckResponse(response, "setup", context);
-            await Task.Delay(TimeSpan.FromSeconds(3), Xunit.TestContext.Current.CancellationToken);
         }
 
         output.WriteLineTimestampedAndDebug("Now going to the tenant landing page to assert the setup.");
