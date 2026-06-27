227ebc5de2c283abe3452b0ce4b68d85d9051e26
Sára El-Saig <sara.el-saig@lombiq.com>
2026-06-27 03:55:44 +0200

    Get rid of unused private method.

--- Lombiq.OrchardCoreApiClient.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.OrchardCoreApiClient.Tests.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -565,7 +565,4 @@ public static class TestCaseUITestContextExtensions
             $"Tenant {taskName} failed with status code {response.StatusCode}. Content: {content}\n" +
             $"Request: {request}\nDriver URL: {context.Driver.Url}");
     }
-
-    private static string GetApiErrorContent(ApiResponse<string> response) =>
-        response.Error is ApiException apiException ? apiException.Content : null;
 }
